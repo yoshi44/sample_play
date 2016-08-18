96b6464478cbf6e05b7b37fbd68a24a75571bbb2
yoshiVirtualBox <yoshivirtualbox@yoshiVirtualBoxs-MacBook-Pro.local>
2016-08-19 06:57:53 +0900

    [branch_name]

--- sample_play_minimal-scala/build.sbt
+++ sample_play_minimal-scala/build.sbt
@@ -5,8 +5,11 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 // Change this to another test framework if you prefer
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-
+//libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "com.typesafe.play" % "play-ws_2.11" % "2.4.8"
+)
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
 
--- sample_play_minimal-scala/src/main/scala/com/example/Hello.scala
+++ sample_play_minimal-scala/src/main/scala/com/example/Hello.scala
@@ -1,7 +1,23 @@
 package com.example
 
+import com.ning.http.client._
+import play.api.Play.current
+import play.api.libs.ws.ning._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
+    //println(s"args(0)===${args(0)}")
+    //println(s"args(1)===${args(1)}")
+
+    val builder = new AsyncHttpClientConfig.Builder()
+    val client = new NingWSClient(builder.build())
+    val wsResponse = client.url("http://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060").get()
+
+    wsResponse.map { response =>
+      println(response.body)
+    }
+
   }
 }
