d7eaf934d78f4a7676ee48a4a18829ac7871eafa
yoshiVirtualBox <yoshivirtualbox@yoshiVirtualBoxs-MacBook-Pro.local>
2016-08-23 06:48:29 +0900

    [branch_name] ws 2.5 logic

--- sample_play_minimal-scala/build.sbt
+++ sample_play_minimal-scala/build.sbt
@@ -4,11 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-// Change this to another test framework if you prefer
-//libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-  "com.typesafe.play" % "play-ws_2.11" % "2.4.8"
+  "com.typesafe.play" % "play-ws_2.11" % "2.5.5"
 )
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
--- sample_play_minimal-scala/src/main/scala/com/example/Hello.scala
+++ sample_play_minimal-scala/src/main/scala/com/example/Hello.scala
@@ -1,9 +1,17 @@
 package com.example
 
-import com.ning.http.client._
-import play.api.Play.current
-import play.api.libs.ws.ning._
-import scala.concurrent.ExecutionContext.Implicits.global
+// 2.4
+//import com.ning.http.client._
+//import play.api.Play.current
+//import play.api.libs.ws.ning._
+//import scala.concurrent.ExecutionContext.Implicits.global
+
+// 2.5
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import play.api.libs.ws.ahc.AhcWSClient
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 object Hello {
   def main(args: Array[String]): Unit = {
@@ -11,12 +19,20 @@ object Hello {
     //println(s"args(0)===${args(0)}")
     //println(s"args(1)===${args(1)}")
 
-    val builder = new AsyncHttpClientConfig.Builder()
-    val client = new NingWSClient(builder.build())
+    // 2.4
+    //val builder = new AsyncHttpClientConfig.Builder()
+    //val client = new NingWSClient(builder.build())
+
+    // 2.5
+    implicit val actorSystem = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+
+    val client = AhcWSClient()
     val wsResponse = client.url("http://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060").get()
 
     wsResponse.map { response =>
       println(response.body)
+      client.close()
     }
 
   }
